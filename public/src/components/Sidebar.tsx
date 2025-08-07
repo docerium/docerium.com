fb905146c6931179263fbbc5caa7ce86f232732d
drew <me@andrinoff.com>
2025-08-08 03:57:26 +0400

    feat: sidebar added

---
+++ public/src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import "@/styles/sidebar.scss";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Sidebar</h2>
+      <p>This is the sidebar content.</p>
+      {/* Add more sidebar content here */}
+    </div>
+  );
+}
