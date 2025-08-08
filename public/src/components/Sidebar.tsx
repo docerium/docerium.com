18b997863ff5d871f309d4b72a30d533ac148e7c
drew <me@andrinoff.com>
2025-08-08 04:30:15 +0400

    feat: github link

--- public/src/components/Sidebar.tsx
+++ public/src/components/Sidebar.tsx
@@ -1,18 +1,22 @@
 import "@/styles/sidebar.scss";
 import { ProjectIcon } from "@primer/octicons-react";
 import { GraphIcon } from "@primer/octicons-react";
+import { MarkGithubIcon } from "@primer/octicons-react";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar-icons">
         <a href="/whiteboard" className="sidebar-icon">
-          <ProjectIcon size={56} />
+          <ProjectIcon size={42} />
         </a>
         <a href="/graphs" className="sidebar-icon">
-          <GraphIcon size={56} />
+          <GraphIcon size={42} />
         </a>
       </div>
+      <a href="https://github.com/andrinoff/tme" className="github-link">
+        <MarkGithubIcon size={42} />
+      </a>
     </div>
   );
 }
