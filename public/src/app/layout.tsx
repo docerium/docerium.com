e2dbd368d9432eee6cba8a85bb01bfde86711996
drew <me@andrinoff.com>
2025-08-09 18:16:23 +0400

    feat: new dock

--- public/src/app/layout.tsx
+++ public/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.scss";
 import "../styles/app.scss";
-import Sidebar from "@/components/Sidebar";
+import DockElement from "@/components/DockElement";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,7 +39,7 @@ export default function RootLayout({
       >
         <div className="layout">
           {children}
-          <Sidebar />
+          <DockElement />
         </div>
       </body>
     </html>
---
+++ public/src/components/DockElement.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Dock from "./Dock";
+import { ProjectIcon, GraphIcon, FileIcon } from "@primer/octicons-react";
+
+const items = [
+  {
+    icon: <ProjectIcon size={18} />,
+    label: "Whiteboard",
+    onClick: () => (window.location.href = "/whiteboard"),
+  },
+  {
+    icon: <GraphIcon size={18} />,
+    label: "Graphs",
+    onClick: () => (window.location.href = "/graphs"),
+  },
+  {
+    icon: <FileIcon size={18} />,
+    label: "Notebook",
+    onClick: () => (window.location.href = "/notebook"),
+  },
+];
+
+export default function DockElement() {
+  return (
+    <Dock items={items} panelHeight={68} baseItemSize={50} magnification={70} />
+  );
+}
